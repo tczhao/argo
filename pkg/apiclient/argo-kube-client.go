8c1cee2e822378ba03c429bd8c3fb7a30b0117a1
heidongxianhua <18207133434@163.com>
2024-04-28 13:45:28 +0800

    fix: retry large archived wf. Fixes #12740 (#12741)
    Signed-off-by: heidongxianhua <18207133434@163.com>

--- pkg/apiclient/argo-kube-client.go
+++ pkg/apiclient/argo-kube-client.go
@@ -85,7 +85,7 @@ func newArgoKubeClient(ctx context.Context, clientConfig clientcmd.ClientConfig,
 
 func (a *argoKubeClient) NewWorkflowServiceClient() workflowpkg.WorkflowServiceClient {
 	wfArchive := sqldb.NullWorkflowArchive
-	wfaServer := workflowarchive.NewWorkflowArchiveServer(wfArchive)
+	wfaServer := workflowarchive.NewWorkflowArchiveServer(wfArchive, argoKubeOffloadNodeStatusRepo)
 	return &errorTranslatingWorkflowServiceClient{&argoKubeWorkflowServiceClient{workflowserver.NewWorkflowServer(a.instanceIDService, argoKubeOffloadNodeStatusRepo, wfaServer)}}
 }
 
